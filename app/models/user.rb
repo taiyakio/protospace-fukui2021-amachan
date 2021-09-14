bc210f944b1428631bedfe0219a5770a99cb7b4b
taiyakio <itaiki1998@gmail.com>
2021-09-14 14:17:09 +0900

    新規登録のバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :profile, :affiliation, :director, presence: true
 end
