e740656d36b0490b70f21f38c24dd814709cb013
YamabeKouta <89598222+YamabeKouta@users.noreply.github.com>
2021-09-18 00:18:12 +0900

    変数の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @profile = user.profile
-    @affiliation =user.affiliation
-    @director = user.director
-    @prototypes = user.prototypes
+    @user = User.find(params[:id])
   end
 end
