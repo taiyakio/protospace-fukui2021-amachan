b5dab3d0e6e84d6254b3a23755959b1c8e7d911b
YamabeKouta <89598222+YamabeKouta@users.noreply.github.com>
2021-09-17 09:47:47 +0900

    コントローラー設計

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    @name = user.name
+    @profile = user.profile
+    @affiliation =user.affiliation
+    @director = user.director
+    @tweets = user.tweets
+  end
 end
