e754a25076c01114a875ae9a68a2eef136efa96b
YamabeKouta <89598222+YamabeKouta@users.noreply.github.com>
2021-09-19 22:07:51 +0900

    プロトタイプ削除に伴うコメントの削除

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_one_attached :image
 
   validates :name, :catch_copy, :concept, :image, presence: true
