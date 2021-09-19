8921ce5c4fc92af00502a57468a37811fb8c221a
YamabeKouta <89598222+YamabeKouta@users.noreply.github.com>
2021-09-19 11:20:50 +0900

    コントローラー・ビューの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to "/prototypes/#{comment.prototype.id}"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
 end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -29,6 +29,8 @@ class PrototypesController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   private
