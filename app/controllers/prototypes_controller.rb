05e29080ab55ca9ad428e252f855f06c433ab09c
YamabeKouta <89598222+YamabeKouta@users.noreply.github.com>
2021-09-25 16:39:21 +0900

    並び替え変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,7 +4,7 @@ class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @prototypes = Prototype.includes(:user)
+    @prototypes = Prototype.includes(:user).order("created_at DESC")
   end
 
   def new
